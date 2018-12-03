252cce55838e40423ee4d3a3dd97e3cea473c73d
deyveson <deyvesonn@outlook.com>
2018-12-03 10:52:04 -0400

    Utilizando o algoritmo MultinomialNB

--- classifica_acessos.py
+++ classifica_acessos.py
@@ -1,6 +1,12 @@
 from dados import carregar_acessos
+
 x, y = carregar_acessos()
 
-print(y)
-print(x)
+from sklearn.naive_bayes import MultinomialNB
+
+modelo = MultinomialNB()
+
+modelo.fit(x, y)
+
+print (modelo.predict([[1,0,1], [0,1,0], [1,0,0], [1,1,0], [1,1,1]]))
 
