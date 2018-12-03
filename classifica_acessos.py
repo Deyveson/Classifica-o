16449e6e6e99e8ad0ad60807ac8f0f060a3a8c55
deyveson <deyvesonn@outlook.com>
2018-12-03 11:23:49 -0400

    Treinando e testando o algoritmo com metade dos dados

--- classifica_acessos.py
+++ classifica_acessos.py
@@ -2,21 +2,25 @@ from dados import carregar_acessos
 
 x, y = carregar_acessos()
 
+treino_dados = x[:90]
+treino_marcacoes = y[:90]
+
+teste_dados = x[-9:]
+teste_marcacoes = y[-9:]
+
 from sklearn.naive_bayes import MultinomialNB
 
 modelo = MultinomialNB()
 
-modelo.fit(x, y)
+modelo.fit(treino_dados, treino_marcacoes)
 
-print (modelo.predict([[1,0,1], [0,1,0], [1,0,0], [1,1,0], [1,1,1]]))
+resultado = modelo.predict(teste_dados)
 
-resultado = modelo.predict(x)
-
-diferencas = resultado - y
+diferencas = resultado - teste_marcacoes
 
 acertos = [d for d in diferencas if d == 0]
 total_de_acertos = len(acertos)
-total_de_elementos = len(x)
+total_de_elementos = len(teste_dados)
 taxa_de_acertos = 100.0 * total_de_acertos / total_de_elementos
 
 print(taxa_de_acertos)
